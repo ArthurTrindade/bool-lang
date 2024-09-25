c5cdc17175d0dd29816ee74876681bff438c16c7
ArthurTrindade <arthsilva500@gmail.com>
2024-09-25 16:57:52 -0300

    add: examples regex

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.util.regex.*;
+
 public class Main {
+    enum Instructions {
+        STD, RET, ATR
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World em outra branch");
+
+        String line = "class Pessoa";
+
+        switch (test(line)) {
+            case RET -> System.out.println("Linha: Return");
+            case ATR -> System.out.println("Linha: atribuição");
+            default -> System.out.println("linha");
+        }
+
+    }
+
+    public static Instructions test(String line) {
+        if (regexReturn(line)) {
+            return Instructions.RET;
+        } else if (atriRegex(line)) {
+            return Instructions.ATR;
+        }
+        return Instructions.STD;
+    }
+
+    public static boolean atriRegex(String text) {
+
+        Pattern pattern = Pattern.compile(Regex.ATR);
+        Matcher matcher = pattern.matcher(text);
+
+        return matcher.find();
+    }
+
+    public static boolean regexReturn(String text) {
+
+        Pattern pattern = Pattern.compile(Regex.RET);
+        Matcher matcher = pattern.matcher(text);
+
+        return matcher.find();
     }
 }
+
+
---
+++ src/Regex.java
@@ -0,0 +1,4 @@
+public class Regex {
+    final static String RET = "return\\s+([a-zA-Z]+)";
+    final static String ATR = "(.+)\\s+=\\s+(.+)";
+}
