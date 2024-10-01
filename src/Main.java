610a1c6ee8449ff0d8c27fdd96dec2f1b988eac9
BrianMath <brianmatheus10@gmail.com>
2024-10-01 13:08:30 -0300

    Milagres

--- src/Main.java
+++ src/Main.java
@@ -22,9 +22,13 @@ public class Main {
 	
 	// Regexes
 	final static String[] regexes = {
-			"(\\s*)return\\s+([a-zA-Z]+)",											// return
-			"(\\s*)([a-zA-Z]+)\\s+=\\s+([a-zA-Z|0-9]+)",							// attribution
-			"\\s*if\\s+([a-zA-Z]+)\\s+([a-zA-Z]+)\\s+([a-zA-Z]+)\\s+then"			// if
+			"(\\s*)return\\s+([a-zA-Z]+)",															// return
+			"(\\s*)([a-zA-Z]+)\\.?([a-zA-Z]+)?\\s+=\\s+([a-zA-Z|0-9]+)\\.?([a-zA-Z]+)?",							// attribution
+			"\\s*if\\s+([a-zA-Z]+)\\s+([a-zA-Z]+)\\s+([a-zA-Z]+)\\s+then",							// if
+			"(\\s*)([a-zA-Z]+)\\s+=\\s+([a-zA-Z]+)\\s+([+*/-])\\s+([a-zA-Z]+)",
+			"(\\s*)([a-zA-Z]+)\\s+=\\s+new\\s+([a-zA-Z]+)",
+			"(\\s*)([a-zA-Z]+)\\s+=\\s+([a-zA-Z]+)\\.([a-zA-Z]+)\\(([^)]*)\\)",
+			"(\\s*)()([a-zA-Z]+)\\.([a-zA-Z]+)\\(([^)]*)\\)"
 	};
 	
 	public static boolean isNumber(String term) throws IOException {
@@ -44,6 +48,8 @@ public class Main {
 	public static List<String> compile(String line) throws IOException {
 		List<String> str = new ArrayList<>();
 		
+		boolean matched = false;
+		
 		// Comparar com todos os regexes
 		for (int i = 0; i < regexes.length; i++) {
 			Pattern pattern = Pattern.compile(regexes[i]);
@@ -55,22 +61,33 @@ public class Main {
 			if (i == 0) {
 				str.add(matcher.group(1) + "load " + matcher.group(2));
 				str.add(matcher.group(1) + "ret");
+				matched = true;
 				break;
 			}
 			
 			// attribution
 			if (i == 1) {
 				String lhs = matcher.group(2);
-				String rhs = matcher.group(3);
+				String rhs = matcher.group(4);
 				
 				if (isNumber(rhs)) {
 					str.add(matcher.group(1) + "const " + rhs);
 				} else if (isName(rhs)) {
 					str.add(matcher.group(1) + "load " + rhs);
+					
+					if (matcher.group(5) != null) {
+						str.add(matcher.group(1) + "get " + matcher.group(5));
+					}
 				}
 				
-				str.add(matcher.group(1) + "store " + lhs);
+				if (matcher.group(3) != null) {
+					str.add(matcher.group(1) + "load " + lhs);
+					str.add(matcher.group(1) + "set " + matcher.group(3));
+				} else {
+					str.add(matcher.group(1) + "store " + lhs);
+				}
 				
+				matched = true;
 				break;
 			}
 			
@@ -112,7 +129,74 @@ public class Main {
 				
 				int indexIf = str.indexOf("if 0");
 				str.set(indexIf, "if " + countIf);
+				
+				matched = true;
+				break;
 			}
+			
+			// Operações aritméticas
+			if (i == 3) {
+				str.add(matcher.group(1) + "load " + matcher.group(3));
+				str.add(matcher.group(1) + "load " + matcher.group(5));
+				switch (matcher.group(4)) {
+					case "+":
+						str.add(matcher.group(1) + "add");
+						break;
+					case "-":
+						str.add(matcher.group(1) + "sub");
+						break;
+					case "*":
+						str.add(matcher.group(1) + "mul");
+						break;
+					case "/":
+						str.add(matcher.group(1) + "div");
+						break;
+					default:
+						break;
+				}
+				
+				str.add(matcher.group(1) + "store " + matcher.group(2));		
+				
+				matched = true;
+				break;
+			}
+			
+			// Object creation
+			if (i == 4) {
+				str.add(matcher.group(1) + "new " + matcher.group(3));
+				str.add(matcher.group(1) + "store " + matcher.group(2));
+				
+				matched = true;
+				break;
+			}
+			
+			//    1        2                     3            4          5
+			// "(\\s*)([a-zA-Z]+)?\\s+=?\\s+([a-zA-Z]+)\\.([a-zA-Z]+)\\((.)\\)"
+			// Functions
+			if (i == 5 || i == 6) {
+				String aux = matcher.group(5);
+				if (!aux.isEmpty()) {
+					String[] variArray = aux.split(", ");
+					
+					for (String vari : variArray) {
+						str.add(matcher.group(1) + "load " + vari);
+					}
+				}	
+				
+				str.add(matcher.group(1) + "load " + matcher.group(3));
+				str.add(matcher.group(1) + "call " + matcher.group(4));
+				
+				if (i == 5) {
+					str.add(matcher.group(1) + "store " + matcher.group(2));
+				}
+					
+				matched = true;
+				break;
+			}
+		}
+		
+		if (!matched) {
+			str.add(line);
 		}
 		
 		return str;
@@ -158,7 +242,4 @@ public class Main {
 		
 		return newName;
 	}
-	
-	
 }
-
